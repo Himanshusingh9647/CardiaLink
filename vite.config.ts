e8578863935492ac1353ab0e51e77b710cb51fcc
Himanshusingh9647 <himanshusingh1088@gmail.com>
2025-05-17 11:51:33 +0530

    Simplify GitHub Pages deployment configuration

--- vite.config.ts
+++ vite.config.ts
@@ -17,20 +17,6 @@ export default defineConfig({
     outDir: 'dist',
     assetsDir: 'assets',
     sourcemap: false,
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-      },
-    },
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-        assetFileNames: 'assets/[name].[hash][extname]',
-        chunkFileNames: 'assets/[name].[hash].js',
-        entryFileNames: 'assets/[name].[hash].js',
-      },
-    },
+    minify: true,
   },
 })
